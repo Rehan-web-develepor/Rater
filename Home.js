8f1c7720b1ab62299e6255038bdd6401989a6e17
Rehan-web-develepor <106889349+Rehan-web-develepor@users.noreply.github.com>
2022-06-05 16:31:31 +0500

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,6 @@
+const side,menu,bg = document.querySelector('.side-menu-bg');  
+      const three_lines_menu = document.querySelector('.three_lines_menu');
+      side-menu-bg.onclick;function(){
+        three_lines_menu.classList.toggle('whenactive')
+      }
+
